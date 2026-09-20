f459cc976b93247eec0ee6901663bf00f5506466
Hardickkkk <hardickchoudhary7@gmail.com>
2026-09-20 19:18:29 +0530

    Time: 1 ms (24.25%) | Memory: 9.8 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum =0;
+        for(int i =1;i<=s.size();i++){
+            sum+=('z'-s[i-1]+1)*i;
+        }
+        return sum;
+        
+    }
+};
